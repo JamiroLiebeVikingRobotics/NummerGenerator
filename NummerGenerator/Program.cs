8aa2623f5b327507fa20c7e9d48f21a7d8885fc3
JamiroLiebeVikingRobotics <jamlie707@gmail.com>
2019-09-06 09:47:17 +0200

    Lade till nummer generator och loop fär användaren

---
+++ NummerGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NummerGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Genererar och sparar ett nummer mellan 0-100
+            Random rnd = new Random();
+            int num = rnd.Next(100);
+
+            Boolean status = false;
+
+            Console.WriteLine("Guess the number between 0-100");
+
+            while (status == false)
+            {
+                Console.WriteLine("Please enter a number");
+
+                string input = Console.ReadLine();
+                int guess = int.Parse(input);
+
+                //Ändrar "Status" om gissningen är rätt för att avsluta Loopen
+                if (guess == num)
+                {
+                    status = true;
+                    Console.WriteLine("Congrats you won!! ");
+                }
+                 
+                
+        
+
+            }
+
+        }
+    }
+}
